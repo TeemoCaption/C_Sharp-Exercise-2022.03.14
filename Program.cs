248656404b5588f93959d56e2fc8a15b26cef615
teemocaption <eyan@gmail.com>
2022-03-14 10:10:19 +0800

    ok

--- Program.cs
+++ Program.cs
@@ -10,7 +10,25 @@ namespace C_Sharp_Exercise_2022._03._14_
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("請輸入數字：　");
+            string num=Console.ReadLine();
+            int x = Convert.ToInt32(num);
+            if(x%2 != 0)
+            {
+                Console.WriteLine("Weird");
+            }
+            else if (x%2 ==0 && x>=2 &&x<=5)
+            {
+                Console.WriteLine("Not Weird");
+            }
+            else if (x % 2 == 0 && x >= 6 && x <= 20)
+            {
+                Console.WriteLine("Weird");
+            }
+            else if(x % 2 == 0)
+            {
+                Console.WriteLine("Not Weird");
+            }
         }
     }
 }
