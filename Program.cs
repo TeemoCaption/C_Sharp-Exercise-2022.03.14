200d688129bde4aa034f0de43e45335c4291837c
teemocaption <eyan@gmail.com>
2022-03-14 11:08:40 +0800

    ok

--- Program.cs
+++ Program.cs
@@ -17,20 +17,46 @@ namespace C_Sharp_Exercise_2022._03._14_
 
             if (x % 2 != 0)
             {
-                Console.WriteLine("Weird");
+                Console.WriteLine("Weird\n");
             }
             else if (x % 2 == 0 && x >= 2 && x <= 5)
             {
-                Console.WriteLine("Not Weird");
+                Console.WriteLine("Not Weird\n");
             }
             else if (x % 2 == 0 && x >= 6 && x <= 20)
             {
-                Console.WriteLine("Weird");
+                Console.WriteLine("Weird\n");
             }
             else if (x % 2 == 0)
             {
-                Console.WriteLine("Not Weird");
+                Console.WriteLine("Not Weird\n");
             }
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0;j<=i;j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine(" ");
+            }
+
+            for (int k = 1; k <= 3; k++)
+            {
+                Console.WriteLine($"{x} * {k} = {x*k}");
+            }
+            Console.WriteLine("\n");
+
+            for (int r=1;r <= 9; r++)
+            {
+                for(int g = 1; g <= 9; g++)
+                {
+                    Console.Write($"{r} * {g} = {r*g}\t");
+                }
+                Console.Write("\n");
+            }
+          
+            Console.Read();
         }
     }
 }
